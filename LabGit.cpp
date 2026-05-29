1dec589ed08d724ddc8c42b998b706eb99d71d91
elmastin <emontenegror@unal.edu.co>
2026-05-29 00:30:30 -0500

    se añadieron las lineas al archivo de texto y el codigo necesario para leerlo

--- LabGit.cpp
+++ LabGit.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <ftream>
+#include <sstream>
 
 using namespace std;
-int main() {
-	
-	
+int main () {
+    ifstream entrada ("input/entrada.txt");
+    
+    if (entrada.is_open()) {
+       cout<<"archivo abierto"<<endl;
+       string linea;
+       
+       while (getline (entrada, linea)) {
+             
+             }
+       }
 }
+                          
+                          
+                          
+                          
+    
+                          
