81ec3d5d72b4c0538f3ad8e5a1d8d780ed436a91
proenelfree <dpulidof@unal.edu.co>
2026-05-29 00:56:12 -0500

    Añadidas lineas para escribir en archivo

--- LabGit.cpp
+++ LabGit.cpp
@@ -5,14 +5,16 @@
 using namespace std;
 int main () {
     ifstream entrada ("input/entrada.txt");
+    ofstream salida ("output/salida.txt");
     
-    if (entrada.is_open()) {
+    if (entrada.is_open() && salida.is_open()) {
        cout<<"archivo abierto"<<endl;
        string linea;
        
        while (getline (entrada, linea)) {
-             
+             salida << linea;
              }
+             cout<<"Archivo copiado correctamente"<<endl;
        }
 }
                           
